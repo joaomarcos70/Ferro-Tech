81977a22ab6456cf69674a2d2729f21eb92611a5
joão <joaomarcos70outlook.com>
2020-10-05 17:04:50 -0300

    inserindo nodemon no package

--- backend/Routes/router.js
+++ backend/Routes/router.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get('', (req, res) => {
-    res.send('obaaa')
+    res.send('seila vei')
 })
 
 module.exports = router;
