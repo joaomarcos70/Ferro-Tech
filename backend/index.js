58f98c1464bbd68cc58005d615f7c0146da89a77
joão <joaomarcos70outlook.com>
2020-10-05 16:58:43 -0300

    criando rotas + git ignore backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
-console.log("teste back");
+const express = require('express')
+const app = express()
+const port = 3000
+
+const router = require('./Routes/router')
+
+app.use("/", router);
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
